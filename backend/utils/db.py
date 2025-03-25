185a858bd61f645c9f10a2388e585a08415a32c8
SalsabeelSaleh <salsabeelmurad@gmail.com>
2025-03-26 02:54:36 +0300

    Update db.py

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,3 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.String(20))
+    age = db.Column(db.Integer)
+    language = db.Column(db.String(5))
