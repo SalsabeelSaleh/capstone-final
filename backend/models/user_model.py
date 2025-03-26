e6b7d1762985860615aed5cb36eb4ca1098ca88c
SalsabeelSaleh <salsabeelmurad@gmail.com>
2025-03-26 03:02:57 +0300

    Update user_model.py

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,6 +1,12 @@
 from utils.db import db
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.String(20))
+    age = db.Column(db.Integer)
+    language = db.Column(db.String(5))
