e39446835121ee23e4ed0e6caeaccf0a37fce10f
Hessa-N <halnajashi@alfaisal.edu>
2025-04-24 01:27:33 +0300

    Update App.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
+import AboutMain from "./components/AboutMain";  // Correct import for AboutMain
+import AboutHome from "./components/AboutHome";  // Correct import for AboutHome
 import EmotionDetection from "./components/EmotionDetection";
 import { AuthProvider } from "./context/AuthContext";
 import CreateAccount from "./components/CreateAccount";
 import LoginPage from "./components/LoginPage";
 import MainPage from "./components/MainPage";
-import Avatar from "./components/Avatar"; // Import Avatar component
-import NextPage from "./components/NextPage"; // Import the NextPage component for selected avatar display
-import Profile from "./components/Profile"; // Import Profile component
-import HelpPage from "./components/Help"; // Import HelpPage component
+import Avatar from "./components/Avatar";
+import NextPage from "./components/NextPage"; 
+import Profile from "./components/Profile"; 
+import HelpPage from "./components/Help";
 
 function App() {
   return (
@@ -19,15 +20,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/aboutmain" element={<AboutMain />} />  
+          <Route path="/abouthome" element={<AboutHome />} />  
           <Route path="/emotion-detection" element={<EmotionDetection />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/main-page" element={<MainPage />} />
-          <Route path="/avatar" element={<Avatar />} /> 
+          <Route path="/avatar" element={<Avatar />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/Help" element={<HelpPage />} />
-          {/* Add NextPage route */}
-          <Route path="/next-page" element={<NextPage />} /> {/* This route will show the selected avatar */}
+          <Route path="/next-page" element={<NextPage />} />
         </Routes>
       </Router>
     </AuthProvider>
